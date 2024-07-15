10a1b806c57097050ad5d51e2649a9e23773425b
VigularIgnat <ignatvigular@gmail.com>
2024-07-15 22:57:02 +0200

    adding some changes

---
+++ src/frontend/src/main.js
@@ -0,0 +1,24 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+
+import App from './App.vue'
+import router from './router'
+
+import VueGoogleMaps from '@fawmi/vue-google-maps';
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.use(VueGoogleMaps, {
+    load: {
+        key: 'api_key',
+        libraries: "places"
+    }
+})
+
+app.mount('#app')
